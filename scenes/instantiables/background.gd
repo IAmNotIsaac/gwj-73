33f4eaed0801605f6ef3029cef76b23451776735
Isaac Astell <isaacjulianastell@gmail.com>
2024-09-18 19:33:53 -0700

    better background tooling

--- scenes/instantiables/background.gd
+++ scenes/instantiables/background.gd
@@ -1,1 +1,27 @@
+@tool
+class_name Background
 extends Node2D
+
+
+enum Type {
+	SHAPES,
+	SPACE,
+	NONE,
+}
+
+@export var background := Type.SHAPES:
+	set(v):
+		background = v
+		_update_background()
+
+
+@onready var _shapes := $Shapes
+@onready var _space := $Space
+
+
+func _update_background() -> void:
+	for child in get_children():
+		child.hide()
+	
+	if background != Type.NONE:
+		get_child(background).show()
